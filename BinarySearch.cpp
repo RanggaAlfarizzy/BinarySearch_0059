6bc4966a8b441f72d3ffa991af94492137189e65
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-07 15:18:55 +0800

    Membuat perulangan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -100,3 +100,8 @@ void binarySearch()
         {
             cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
         }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
+}
