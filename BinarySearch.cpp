9e213208c7d444614157cd39e0655cea9f7e2acb
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-07 15:15:11 +0800

    Mengatur batas high dan low

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -86,3 +86,12 @@ void binarySearch()
                 cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
                 return;
             }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
